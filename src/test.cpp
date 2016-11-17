a67f619a9cf9e049774703e65784b01ad6fb3957
Ignacio Perez-Hurtado <iperde@upo.es>
2016-11-17 06:45:35 +0100

    Initial version

---
+++ src/test.cpp
@@ -0,0 +1,31 @@
+/***********************************************************************/
+/**                                                                    */
+/** test.cpp                                                           */
+/**                                                                    */
+/** Copyright (c) 2016, Service Robotics Lab.                          */ 
+/**                     http://robotics.upo.es                         */
+/**                                                                    */
+/** All rights reserved.                                               */
+/**                                                                    */
+/** Authors:                                                           */
+/** Ignacio Perez-Hurtado (maintainer)                                 */
+/** Jesus Capitan                                                      */
+/** Fernando Caballero                                                 */
+/** Luis Merino                                                        */
+/**                                                                    */   
+/** This software may be modified and distributed under the terms      */
+/** of the BSD license. See the LICENSE file for details.              */
+/**                                                                    */
+/** http://www.opensource.org/licenses/BSD-3-Clause                    */
+/**                                                                    */
+/***********************************************************************/
+
+#include <iostream>
+#include <sfm.hpp>
+
+int main()
+{
+	sfm::SFM;
+	std::cout<<"test"<<std::endl;
+	return 0;
+}
